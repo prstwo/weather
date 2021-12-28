1a14153a01b3725bef7bc90cfb5f250265cefc0d
P Alirezazade <p.alirezazade@webmehr.com>
2021-12-28 13:43:01 +0330

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+ // const key ='ac89a533b6e0b6c6f66719241b35ea45';
+ export default {
+   name:"Card",
+   data(){
+     return{
+
+     }
+   }
+ }
+</script>
+<style></style>
